e1d70e7d55d6880c101211e8075591c6b469053f
punnja <punnja@sheridancollege.ca>
2020-06-02 19:06:07 -0400

    first commit

---
+++ src/week2tuesday/Week2tuesday.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2tuesday;
+
+/**
+ *
+ * @author jaska
+ */
+public class Week2tuesday {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hi");
+    
+        // TODO code application logic here
+    }
+    
+}
